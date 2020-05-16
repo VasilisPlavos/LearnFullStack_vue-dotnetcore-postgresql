5d905e091389d5d1539c2ac9d9e98bfc6f6f0aeb
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2020-05-16 23:15:48 +0300

    frontend

---
+++ SolarCoffee/solarcoffee.frontend/src/components/SideMenu.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="side-menu-container">
+    <p>test</p>
+</div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    name: 'SideMenu',
+    components: {}
+})
+
+export default class SideMenu extends Vue { }
+</script>
+
+<style scoped lang="scss">
+
+.side-menu-container {
+    background-color: aqua;
+}
+    
+</style>
